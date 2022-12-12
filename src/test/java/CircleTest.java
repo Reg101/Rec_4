81cd47eb53ece7d0e4b71f5cbe190acd7e3e4405
Reg101 <ricarter21@students.desu.edu>
2022-12-11 19:23:13 -0500

    Updated code

--- src/test/java/CircleTest.java
+++ src/test/java/CircleTest.java
@@ -1,7 +1,9 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CircleTest {
     private static Random rand;
